0db80cda71d849f94e7cb2395f4047b0ec9dc7f3
Joao Victor <84978593+joaovictordds@users.noreply.github.com>
2024-01-15 19:54:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import sklearn
+#import sklearn
 import scikit-learn
 #from sklearn.pipeline import Pipeline
 #from sklearn.ensemble import GradientBoostingClassifier
